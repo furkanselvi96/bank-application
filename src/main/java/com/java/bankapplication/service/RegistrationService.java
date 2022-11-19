02c164481d16ced3aac7e1002e107c1ba9cb3bd9
Furkan Selvi <furkanselvi1996@icloud.com>
2022-11-19 21:52:13 +0300

    project arrangement.

--- src/main/java/com/java/bankapplication/service/RegistrationService.java
+++ src/main/java/com/java/bankapplication/service/RegistrationService.java
@@ -8,15 +8,5 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegistrationService {
 
-    @Autowired
-    UserRepository userRepository;
 
-    public int addAdmin(String name, String username, String accno,
-                        int mobno, String address, int ifsc, int balance) {
-        return userRepository.addAdmin(name, username, accno, mobno, address, ifsc, balance);
-    }
-
-    public int addAdminLogin(String username, String password, String role) {
-        return userRepository.addAdminLogin(username, password, role);
-    }
 }
