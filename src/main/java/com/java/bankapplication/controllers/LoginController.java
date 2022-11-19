ea2080aa704b684b8f0f00afcb012d46c9831811
Furkan Selvi <furkanselvi1996@icloud.com>
2022-11-19 19:50:35 +0300

    optimize import's

--- src/main/java/com/java/bankapplication/controllers/LoginController.java
+++ src/main/java/com/java/bankapplication/controllers/LoginController.java
@@ -4,11 +4,7 @@ import com.java.bankapplication.service.LoginService;
 import com.java.bankapplication.service.Mailservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.IOException;
 
 @Controller
 public class LoginController {
